8c0f0210ee8a7cf1645e7634e5618a3a3d2f466e
turnossync <turnossync@gmail.com>
2024-11-11 12:59:28 -0500

    chore(backend): connect project with render

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-"use strict";
-console.log("Hello World!");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+  console.log("I'm at TURNO-SYNC-BACK/src/index.js")
+});
