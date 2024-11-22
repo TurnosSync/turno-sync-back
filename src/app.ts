95b2a19f3831aaf9a61ff4bf8e5c1aeff262c322
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-11-22 13:34:48 -0500

    Fix conflict

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-// app.ts
 import express from "express";
+import cors from "cors";
 import professionalRoutes from "./interfaces/routes/professionalRoutes";
 import serviceRoutes from "./interfaces/routes/serviceRoutes";
 import snakeToCamelMiddleware from "./interfaces/middlewares/snakeToCamelMiddleware";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow FrontEnd Requests
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(snakeToCamelMiddleware);
